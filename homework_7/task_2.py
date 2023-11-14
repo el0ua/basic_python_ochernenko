9a727ac624887b435b587ab9d5b1eb0f4f94fb6d
Olena Chernenko <ochernenko@bcdtriptech.com>
2023-11-14 12:59:36 +0200

    homework_7 task_2 v.2

--- homework_7/task_2.py
+++ homework_7/task_2.py
@@ -1,9 +1,26 @@
-for number in range(1, 101):
+def fizz_bizz(number):
     if number % 3 == 0 and number % 5 == 0:
-        print('FizzBuzz')
+        return 'FizzBuzz'
     elif number % 3 == 0:
-        print('Fizz')
+        return 'Fizz'
     elif number % 5 == 0:
-        print('Buzz')
+        return 'Buzz'
     else:
-        print(number)
+        return number
+
+
+def main():
+    for number in range(1, 101):
+        result = fizz_bizz(number)
+        print(result)
+
+
+def tests():
+    assert fizz_bizz(3) == 'Fizz'
+    assert fizz_bizz(5) == 'Buzz'
+    assert fizz_bizz(15) == 'FizzBuzz'
+    assert fizz_bizz(7) == 7
+
+if __name__ == '__main__':
+    main()
+    tests()
