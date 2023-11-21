079c044f786d1bdd754128ac42cbf270ff8ab48c
Olena Chernenko <ochernenko@bcdtriptech.com>
2023-11-21 22:34:45 +0200

    homework_8

---
+++ homework_8/task_3.py
@@ -0,0 +1,22 @@
+def sort_by_number_value(list):
+    sorted_list = sorted(list, key=lambda n: float(n))
+    return sorted_list
+
+
+def sort_by_first_digit(list):
+    sorted_list = sorted(list, key=lambda n: str(n)[0])
+    return sorted_list
+
+
+def main():
+    original_list_1 = [5, '9', 0, '452', 6.5, '6', 1, 2]
+    sorted_list_1 = sort_by_number_value(original_list_1)
+    print(f'Список {original_list_1} відсортований за значенням числа елементу: {sorted_list_1}')
+
+    original_list_2 = [472, 326, 1, 999.0, '1101000', '99', 9, '20', 863, '0']
+    sorted_list_2 = sort_by_first_digit(original_list_2)
+    print(f'Список {original_list_2} відсортований за значенням першої цифри числа: {sorted_list_2}')
+
+
+if __name__ == '__main__':
+    main()
