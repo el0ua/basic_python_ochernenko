db80c7c91eb731fef1731a39b1770837e0c15a9c
Olena Chernenko <ochernenko@bcdtriptech.com>
2023-12-27 23:09:25 +0200

    homework_8

---
+++ homework_8/task_2.py
@@ -0,0 +1,23 @@
+def copydeep(obj):
+    if isinstance(obj, (str, int, float, bool)):
+        return obj
+    elif isinstance(obj, list):
+        copy_list = []
+        for i in obj:
+            copy_list.append(copydeep(i))
+        return copy_list
+    elif isinstance(obj, tuple):
+        copy_tuple = ()
+        for i in obj:
+            copy_tuple += copy_tuple(i)
+        return copy_tuple
+
+def main():
+    lst1 = ['a', 1, 2.0, ['b']]
+    lst2 = copydeep(lst1)
+    lst1[3].append(0)
+    print(f"Original list:\n{lst1[3]}\nCopy:\n{lst2[3]}")
+
+
+if __name__ == "__main__":
+    main()
